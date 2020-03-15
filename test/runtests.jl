741e012e7d0b86a709e68a924fec43c6b9e7ccea
Wedg <mwedg@yahoo.co.uk>
2020-03-15 22:08:10 +0200

    Test changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Test
 
 # Run tests
 
-tic()
-println("Test 1")
-@time @test include("plotshape.jl")
-println("Test 2")
-@time @test include("choropleth.jl")
-toc()
+@time begin
+    println("Test 1")
+    @time @test include("plotshape.jl")
+    println("Test 2")
+    @time @test include("choropleth.jl")
+end
