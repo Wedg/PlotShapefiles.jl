377724a83b2462a3e2403f08f10aae55d804991f
Wedg <mwedg@yahoo.co.uk>
2020-03-16 17:53:40 +0200

    Choropleth shape changes

--- src/choropleth.jl
+++ src/choropleth.jl
@@ -5,14 +5,21 @@
 ####################
 
 # Plot choropleth  - given array of shapes and a canvas
-function choropleth(shapes::AbstractArray{T, 1}, canvas::Compose.Context,
-                    fill_data, fill_color_map;
+function choropleth(shparray::Union{AbstractArray{T, 1}, AbstractArray{Union{Missing, T},1}},
+                    canvas::Compose.Context, fill_data, fill_color_map;
                     convertcoords=lonlat_to_webmercator, img_width=12cm,
                     line_width=0.05mm, line_color="black",
                     transform=identity) where {T<:Shapefile.GeoInterface.AbstractGeometry}
 
+    # Check for missings
+    try
+        shparray = disallowmissing(shparray)
+    catch
+        error("Error: Shape array contains missing values")
+    end
+
     # This method is only for polygons
-    @assert typeof(shapes[1]) <: ShpPolygon
+    @assert typeof(shparray[1]) <: ShpPolygon
 
     # Colours
     n_colors = length(fill_color_map)
@@ -22,7 +29,7 @@ function choropleth(shapes::AbstractArray{T, 1}, canvas::Compose.Context,
 
     # Plot outlines to canvas
     i = 1
-    for polygon in shapes
+    for polygon in shparray
         val = transform(Float64(isnan(fill_data[i]) ? 0.0 : fill_data[i]))  # CHECK CHANGE FROM ISNA TO ISNAN !!!
         i += 1
         normval = (val - minval) / range
@@ -39,26 +46,42 @@ function choropleth(shapes::AbstractArray{T, 1}, canvas::Compose.Context,
 end
 
 # Plot choropleth - given array of shapes and an MBR
-function choropleth(shapes::AbstractArray{T, 1}, MBR::Shapefile.Rect,
+function choropleth(shparray::Union{AbstractArray{T, 1}, AbstractArray{Union{Missing, T},1}},
+                    MBR::Shapefile.Rect,
                     fill_data, fill_color_map;
                     convertcoords=lonlat_to_webmercator, img_width=12cm,
                     options...) where {T<:Shapefile.GeoInterface.AbstractGeometry}
+
+    # Check for missings
+    try
+        shparray = disallowmissing(shparray)
+    catch
+        error("Error: Shape array contains missing values")
+    end
 
     # Create canvas
     canvas = create_canvas(MBR, convertcoords, img_width)
 
     # Plot
-    choropleth(shapes, canvas, fill_data, fill_color_map,
+    choropleth(shparray, canvas, fill_data, fill_color_map,
                convertcoords=convertcoords, img_width=img_width; options...)
 end
 
 # Plot choropleth - given an array of shapes
-function choropleth(shapes::AbstractArray{T, 1}, fill_data, fill_color_map;
+function choropleth(shparray::Union{AbstractArray{T, 1}, AbstractArray{Union{Missing, T},1}},
+                    fill_data, fill_color_map;
                     options...) where {T<:Shapefile.GeoInterface.AbstractGeometry}
 
+    # Check for missings
+    try
+        shparray = disallowmissing(shparray)
+    catch
+        error("Error: Shape array contains missing values")
+    end
+
     # Create MBR
-    MBR = minBR(shapes)
+    MBR = minBR(shparray)
 
     # Plot
-    choropleth(shapes, MBR, fill_data, fill_color_map; options...)
+    choropleth(shparray, MBR, fill_data, fill_color_map; options...)
 end
